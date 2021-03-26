56de1b681e25a998a03edee4d7c38c101c9bd78c
Michael Casas <michaelcasas13@gmail.com>
2021-03-26 12:40:19 -0500

    hikariCP and app.properties structure

--- src/main/java/com/michael/tasque/model/Task.java
+++ src/main/java/com/michael/tasque/model/Task.java
@@ -1,16 +1,16 @@
 package com.michael.tasque.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
+@Data
+@Entity
 @Table(name = "tasks")
 public class Task {
 
